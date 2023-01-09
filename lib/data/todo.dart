c26a132ae580e0ee3baeca07258c83228f2af391
seungwoo shin <pumpkinsoft88@gmail.com>
2023-01-09 13:24:23 +0900

    second commit

---
+++ lib/data/todo.dart
@@ -0,0 +1,10 @@
+class Todo {
+  String title;
+  String memo;
+  String category;
+  int color;
+  int done;
+  int date;
+
+  Todo({this.title, this.memo, this.color, this.category, this.done, this.date});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/data/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To Do List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,6 +30,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  List<Todo> todos = [
+    Todo(
+      title: "패스트 캠퍼스 강의듣기",
+      memo: "앱개발 입문강의 듣기",
+      color: Colors.redAccent.value,
+      done: 0,
+      category: "공부",
+      date: 20230109,
+    ),
+    Todo(
+      title: "패스트 캠퍼스 강의듣기2",
+      memo: "앱개발 입문강의 듣기",
+      color: Colors.blue.value,
+      done: 0,
+      category: "공부",
+      date: 20230109,
+    )
+  ];
+
+  @override
+    void initState() {
+      super.initState();
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,8 +62,32 @@ class _MyHomePageState extends State<MyHomePage> {
         preferredSize: const Size.fromHeight(0),
         child: AppBar(),
       ),
-      body: Column(
-        children: const [],
+      body: ListView.builder(
+        itemBuilder: (ctx, idx) {
+          if(idx == 0){
+            return Container(
+              child: Text("오늘하루"),
+            );
+          } else if(idx == 1){
+            return Container(
+              child: Column(
+                children: List.generate(todos.length, (_idx){
+                  Todo t = todos[_idx];
+
+                  return Container(
+                    child: Column(
+                      children: [
+                        Row(children: [
+                          Text(t.title),
+                          Text(t.done == 0 ? "미완료" : "완료")
+                        ],)
+                    ]),
+                  );
+                }),
+            ));
+          }
+        },
+        itemCount: 4,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [

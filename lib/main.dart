10e43bc09766b3366e0d6964e013cc549d6d9449
seungwoo shin <pumpkinsoft88@gmail.com>
2023-01-10 13:23:42 +0900

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
       title: "패스트 캠퍼스 강의듣기2",
       memo: "앱개발 입문강의 듣기",
       color: Colors.blue.value,
-      done: 0,
+      done: 1,
       category: "공부",
       date: 20230109,
     )
@@ -66,27 +66,77 @@ class _MyHomePageState extends State<MyHomePage> {
         itemBuilder: (ctx, idx) {
           if(idx == 0){
             return Container(
-              child: Text("오늘하루"),
+              child: Text("오늘하루", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              margin: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
             );
           } else if(idx == 1){
             return Container(
               child: Column(
                 children: List.generate(todos.length, (_idx){
                   Todo t = todos[_idx];
-
+                  
                   return Container(
+                    decoration: BoxDecoration(
+                      color: Color(t.color),
+                      borderRadius: BorderRadius.circular(16)),
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                    margin: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Row(children: [
-                          Text(t.title),
-                          Text(t.done == 0 ? "미완료" : "완료")
-                        ],)
-                    ]),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                          Text(t.title, style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
+                          Text(t.done == 0 ? "미완료" : "완료", style: TextStyle(color: Colors.white),)
+                        ],
+                        ),
+                        Container(height: 12),
+                        Text(t.memo, style: TextStyle(color: Colors.white),)
+                    ],
+                    ),
+                  );
+                }),
+            ));
+          }
+        
+        else if(idx == 2){
+            return Container(
+              child: Text("완료된 하루", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              margin: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+            );
+          } else if(idx == 3){
+            return Container(
+              child: Column(
+                children: List.generate(todos.length, (_idx){
+                  Todo t = todos[_idx];
+                  
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Color(t.color),
+                      borderRadius: BorderRadius.circular(16)),
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                    margin: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                          Text(t.title, style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),),
+                          Text(t.done == 0 ? "미완료" : "완료", style: TextStyle(color: Colors.white),)
+                        ],
+                        ),
+                        Container(height: 12),
+                        Text(t.memo, style: TextStyle(color: Colors.white),)
+                    ],
+                    ),
                   );
                 }),
             ));
           }
         },
+        
         itemCount: 4,
       ),
       bottomNavigationBar: BottomNavigationBar(
